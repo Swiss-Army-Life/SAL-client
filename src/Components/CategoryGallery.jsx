b7c4bc9af505ce9580b6fe3adbb595cb073ae297
mawesome <kissisme@hotmail.com>
2021-02-06 10:12:31 -0800

    header, footer

--- src/Components/CategoryGallery.jsx
+++ src/Components/CategoryGallery.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React from "react";
 
 function CategoryGallery(props) {
-    return (
-        <div>
-            <h4>Category Gallery</h4>
-        </div>
-    );
+  return <div></div>;
 }
 
 export default CategoryGallery;
+
+// import React, { useState, useEffect } from 'react';
+
+// function CategoryGallery() {
+//     const [category, setCategory] = useState([]);
+
+//     useEffect(()=> {
+//      const url= " https://morning-taiga-97781.herokuapp.com/project/cat/"
+//      fetch(url)
+//      .then((res) => res.json())
+//      .then((json) => {
+//          let category = json;
+//          setCategory(category);
+//      })
+//      .catch(console.error);
+//     }, []);
+//     return (
+//         <section className="cats">
+//             {category.map((category)=> {
+//                 <Link to={`/cat/${category.id}`} key={category.id}>
+//                     <div className="cats">
+//                     <h3>{category.id}</h3>
+//                     </div>
+
+//                 </Link>
+//             })}
+
+//         </section>
+//     );
+// }
+
+// export default CategoryGallery;
