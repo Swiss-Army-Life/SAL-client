5e076067f8c9d7db83cc423f231158539da18048
mawesome <kissisme@hotmail.com>
2021-02-06 11:37:40 -0800

    footer align

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,56 +6,62 @@ function Footer(props) {
   return (
     <div>
       <div className="foot-container">
-        <h5>Kemistry, Inc. 2021</h5>
-      </div>
-      <div className="links">
-        <div className="emaun">
-          <h4>Emaun Hyde</h4>
-          <a
-            href="https://github.com/emaunhyde"
-            target="blank"
-            rel="noreferrer"
-          >
-            <img src={GH} alt="Github" height="60px" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/emaunhyde"
-            target="blank"
-            rel="noreferrer"
-          >
-            <img src={LK} alt="Github" height="60px" />
-          </a>
-        </div>
-        <div className="kel">
-          <h4>Kelpius Zannou</h4>
-          <a href="https://github.com/KelpiusZ" target="blank" rel="noreferrer">
-            <img src={GH} alt="Github" height="60px" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/kelpius-zannou/"
-            target="blank"
-            rel="noreferrer"
-          >
-            <img src={LK} alt="Github" height="60px" />
-          </a>
-        </div>
+        <div className="links">
+          <div className="emaun">
+            <h4>Emaun Hyde</h4>
+            <a
+              href="https://github.com/emaunhyde"
+              target="blank"
+              rel="noreferrer"
+            >
+              <img src={GH} alt="Github" height="60px" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/emaunhyde"
+              target="blank"
+              rel="noreferrer"
+            >
+              <img src={LK} alt="Github" height="60px" />
+            </a>
+          </div>
+          <div className="kel">
+            <h4>Kelpius Zannou</h4>
+            <a
+              href="https://github.com/KelpiusZ"
+              target="blank"
+              rel="noreferrer"
+            >
+              <img src={GH} alt="Github" height="60px" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/kelpius-zannou/"
+              target="blank"
+              rel="noreferrer"
+            >
+              <img src={LK} alt="Github" height="60px" />
+            </a>
+          </div>
 
-        <div className="michaelann">
-          <h4>Michaelann Awesome</h4>
-          <a
-            href="https://github.com/michaelannawesome"
-            target="blank"
-            rel="noreferrer"
-          >
-            <img src={GH} alt="Github" height="60px" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/michaelann-awesome/"
-            target="blank"
-            rel="noreferrer"
-          >
-            <img src={LK} alt="Github" height="60px" />
-          </a>
+          <div className="michaelann">
+            <h4>Michaelann Awesome</h4>
+            <a
+              href="https://github.com/michaelannawesome"
+              target="blank"
+              rel="noreferrer"
+            >
+              <img src={GH} alt="Github" height="60px" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/michaelann-awesome/"
+              target="blank"
+              rel="noreferrer"
+            >
+              <img src={LK} alt="Github" height="60px" />
+            </a>
+          </div>
+        </div>
+        <div className="kemistry">
+          <h5>Kemistry&copy; 2021</h5>
         </div>
       </div>
     </div>
