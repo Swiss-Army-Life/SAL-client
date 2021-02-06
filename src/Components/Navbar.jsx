918ebd86deb30530b19369a9e0399623f6b35868
emaunhyde <emaunaridnihyde@gmail.com>
2021-02-06 13:24:57 -0500

    navbar created

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+//install react-router-dom and import
+import { Link } from "react-router-dom";
 
 function Navbar(props) {
-    return (
-        <div>
-           <h2>Navbar</h2> 
-        </div>
-    );
+  return (
+    <div className="navBar-Container">
+      <h2>Navbar</h2>
+
+      {/* ---- Link to HOME ---- */}
+      <div>
+        <Link to="/">HOME</Link>
+      </div>
+
+      {/* ---- Link to Create A Project ---- */}
+      <div>
+        <Link to="/project/addproject">Create Project</Link>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
