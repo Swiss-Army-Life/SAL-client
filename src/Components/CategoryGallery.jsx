9d71062cbbe0f231bd5b6e321b4586e9d0664151
emaunhyde <emaunaridnihyde@gmail.com>
2021-02-08 16:03:30 -0500

    clean up

--- src/Components/CategoryGallery.jsx
+++ src/Components/CategoryGallery.jsx
@@ -1,41 +1,7 @@
-// import React from "react";
-
-// function CategoryGallery(props) {
-//   const categories = `https://morning-taiga-97781.herokuapp.com/project/cat/{$category}`;
-//   const cat = props.categories.map((category) => {
-//     return <div key={category.id}>{category.id}</div>;
-//   });
-
-//   return (
-//     <section className="catCards">
-//       <h3>{cat}</h3>
-//     </section>
-//   );
-// }
-
-// export default CategoryGallery;
-
-// import React from "react";
-// import axios from 'axios';
-// import { Link } from "react-router-dom";
-
-// function CategoryGallery(props) {
-//     axios.get(`https://morning-taiga-97781.herokuapp.com/project/cat/${category}`)
-//     .then(res => {
-//         const category=
-//     })
-//   return (
-//   <div>
-
-//   </div>)
-// }
-
-// export default CategoryGallery;
-
 import React, { useContext } from "react";
 import SearchContext from "./SearchContext";
 
-const CategoryGallery = (category) => {
+const CategoryGallery = () => {
   const url = "https://morning-taiga-97781.herokuapp.com/project/cat";
   const { searchComplete, setSearchResults, setSearchComplete } = useContext(
     SearchContext
@@ -74,35 +40,5 @@ const CategoryGallery = (category) => {
   }
   return <></>;
 };
-
-// function CategoryGallery() {
-//   const [category, setCategory] = useState("furniture");
-//   const [categoryArray, setCategoryArray] = useState([]);
-
-//   const getCat = async (category) => {
-//     const url = `https://morning-taiga-97781.herokuapp.com/project/cat/${category}`;
-//     console.log(url);
-//     const result = await fetch(url);
-//     const resultjson = await result.json();
-//     console.log(resultjson);
-//     setCategoryArray(resultjson);
-//   };
-//   console.log(category);
-//   useEffect(() => {
-//     getCat(category);
-//   }, []);
-//   return (
-//     <section className="cats">
-//       {categoryArray.map((category) => {
-//         console.log(category);
-//         return (
-//           <div className="cats">
-//             <h3>{category.title}</h3>
-//           </div>
-//         );
-//       })}
-//     </section>
-//   );
-// }
 
 export default CategoryGallery;
