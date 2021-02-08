186edba6133d2e44c8446d8c292ecccba7a3c8fc
michaelannawesome <michaelannawesome@gmail.com>
2021-02-07 19:00:58 -0800

    still stuck

--- src/Components/CategoryGallery.jsx
+++ src/Components/CategoryGallery.jsx
@@ -29,36 +29,70 @@
 // export default CategoryGallery;
 
 import React, { useState, useEffect } from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-function CategoryGallery() {
-  const [category, setCategory] = useState("furniture");
-  const [categoryArray, setCategoryArray] = useState([]);
-
-  const getCat = async (category) => {
-    const url = `https://morning-taiga-97781.herokuapp.com/project/cat/${category}`;
-    console.log(url);
-    const result = await fetch(url);
-    const resultjson = await result.json();
-    console.log(resultjson);
-    setCategoryArray(resultjson);
-  };
-  console.log(category);
-  useEffect(() => {
-    getCat(category);
-  }, []);
-  return (
-    <section className="cats">
-      {categoryArray.map((category) => {
-        console.log(category);
-        return (
-          <div className="cats">
-            <h3>{category.title}</h3>
-          </div>
-        );
-      })}
-    </section>
-  );
+const categories = ["Kitchen", "Lighting", "Furniture","Ceramics", "Food", "Gardening" ]
+const [results, setResults] = useState ({})
+const CategoryGallery= (category) => {
+  const url = `https://morning-taiga-97781.herokuapp.com/project/cat/${category}`;
+  const result = fetch(`url${category}`)
+  setResults(results)
+  setSearchComplete(true) 
 }
+useEffect(() => {
+      getsData(category);
+    }, []);
+return (
+  
+  categories.map((category) => {
+    <Link to="/" onClick={getsData(category)}>
+    <h1>{category}</h1>
+  </Link>
+  
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function CategoryGallery() {
+//   const [category, setCategory] = useState("furniture");
+//   const [categoryArray, setCategoryArray] = useState([]);
+
+//   const getCat = async (category) => {
+//     const url = `https://morning-taiga-97781.herokuapp.com/project/cat/${category}`;
+//     console.log(url);
+//     const result = await fetch(url);
+//     const resultjson = await result.json();
+//     console.log(resultjson);
+//     setCategoryArray(resultjson);
+//   };
+//   console.log(category);
+//   useEffect(() => {
+//     getCat(category);
+//   }, []);
+//   return (
+//     <section className="cats">
+//       {categoryArray.map((category) => {
+//         console.log(category);
+//         return (
+//           <div className="cats">
+//             <h3>{category.title}</h3>
+//           </div>
+//         );
+//       })}
+//     </section>
+//   );
+// }
 
 export default CategoryGallery;
